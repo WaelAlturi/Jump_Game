b0796d124b4af59d6b008016c20865cf6812f8d1
WaelAlturi <101098027+WaelAlturi@users.noreply.github.com>
2024-02-21 13:36:31 +0200

    the game is ready

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const character = document.querySelector(".character");
 const obstacle = document.querySelector(".obstacle");
+const start = document.querySelector(".startButton");
+
+let playerSpeed = 18;
+let enemySpeed = 35000;
+let levelUp = 500;
+
+const scoreBoreder = document.querySelector(".score");
+let score = 0;
 
 function jump() {
   const jumpInterval = setInterval(() => {
     const characterTop = parseInt(
       window.getComputedStyle(character).getPropertyValue("top")
     );
-    if (characterTop > 320) {
+    if (characterTop > 220) {
       character.style.top = characterTop - 8 + "px";
     } else {
       clearInterval(jumpInterval);
@@ -14,39 +22,53 @@ function jump() {
         const characterTop = parseInt(
           window.getComputedStyle(character).getPropertyValue("top")
         );
-        if (characterTop < 450) {
+        if (characterTop < 350) {
           character.style.top = characterTop + 8 + "px";
         } else {
           clearInterval(fallInterval);
         }
-      }, 20);
+      }, playerSpeed);
     }
-  }, 20);
+  }, playerSpeed);
 }
 function gameLoop() {
-  setInterval(() => {
+  character.style.display = "unset";
+  obstacle.style.display = "unset";
+  start.style.display = "none";
+  const game = setInterval(() => {
     const obstacleLeft = parseInt(
       window.getComputedStyle(obstacle).getPropertyValue("left")
     );
-    if (obstacleLeft < -50) {
+    if (obstacleLeft < -250) {
       obstacle.style.transition = "none";
-      obstacle.style.left = "100%";
+      obstacle.style.left = "85%";
+      if (score > levelUp) {
+        enemySpeed += 20000;
+        levelUp += 1000;
+      }
     } else if (conditions()) {
       alert("game Over");
+      score = 0;
+      enemySpeed = 35000;
+      clearInterval(game);
     } else {
-      obstacle.style.transition = "left 2s ease";
-      obstacle.style.left = obstacleLeft - 2500 + "px";
+      obstacle.style.transition = "left 10s ease";
+      obstacle.style.left = obstacleLeft - enemySpeed + "px";
+      scoreBoreder.style.display = "block";
+      score++;
+      scoreBoreder.innerHTML = "SCORE:" + score;
     }
-  }, 30);
+  }, 10);
+  startButton.style.display = "none";
 }
 function conditions() {
   const characterRect = character.getBoundingClientRect();
   const obstacleRect = obstacle.getBoundingClientRect();
   return !(
-    characterRect.bottom < obstacleRect.top ||
-    characterRect.top > obstacleRect.bottom ||
-    characterRect.right < obstacleRect.left ||
-    characterRect.left > obstacleRect.right
+    characterRect.bottom - 20 < obstacleRect.top ||
+    characterRect.top > obstacleRect.bottom - 20 ||
+    characterRect.right - 50 < obstacleRect.left ||
+    characterRect.left > obstacleRect.right - 50
   );
 }
 document.addEventListener("keydown", (event) => {
@@ -54,4 +76,3 @@ document.addEventListener("keydown", (event) => {
     jump();
   }
 });
-gameLoop();
